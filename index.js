44a9af904a207976208d1ebd201e11a22f812625
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-20 04:24:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target")
+}
+
+function increaseRankBy(n) {
+  var rankedLis = document
+  .querySelectorAll("ul.ranked-list li");
+  for (let i=0; i < rankedLis.length; i++) {
+//    rankNum = parseInt(rankedLis[i].innerHTML, 10) + n;
+    rankedLis[i].innerHTML = (parseInt(rankedLis[i].innerHTML, 10) + n).toString()
+  }
+}
